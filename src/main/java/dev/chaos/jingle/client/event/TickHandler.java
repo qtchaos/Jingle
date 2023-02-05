9564c40c3788c7225d0e816f0b6271d660a36828
qtchaos <72168435+qtchaos@users.noreply.github.com>
2023-02-05 16:04:26 +0200

    Improve sound execute accuracy.

--- src/main/java/dev/chaos/jingle/client/event/TickHandler.java
+++ src/main/java/dev/chaos/jingle/client/event/TickHandler.java
@@ -1,17 +1,17 @@
 package dev.chaos.jingle.client.event;
 
-
+import dev.chaos.jingle.client.JingleClient;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-
-import static dev.chaos.jingle.client.JingleClient.PLAY_SOUND;
-import static dev.chaos.jingle.client.JingleClient.alert;
-
+import net.minecraft.client.MinecraftClient;
 
 public class TickHandler {
     public static void register() {
         ClientTickEvents.START_WORLD_TICK.register(world -> {
-            if (PLAY_SOUND) {
-                alert();
+            MinecraftClient client = MinecraftClient.getInstance();
+            if (client.player != null && client.worldRenderer.isRenderingReady(client.player.getBlockPos()) && client.currentScreen == null) {
+                if (JingleClient.PLAY_SOUND) {
+                    JingleClient.alert();
+                }
             }
         });
     }
